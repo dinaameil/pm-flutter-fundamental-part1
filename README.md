af341661ebf596c20517fd11d21097ef660970b1
dinaameil <dinaameil753@gmail.com>
2026-04-07 11:15:50 +0700

    Menerapkan Widget Material Design

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # hello_word
+**Nama:** Dina Mei Lestari  
+**NIM:** 244107060105
 
-A new Flutter project.
-
+## Praktikum Week 5
 ![Screenshot hello_word](image/01.png)
 ![Screenshot hello_word-teks](image/02.png)
 ![Screenshot hello_word-gambar](image/03.png)
+![GIF Alert Dialog](image/pm6.gif)
+*Menerapkan Widget Material Design dan iOS Cupertino*
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-
+// Import semua file latihan kamu
+import 'basic_widgets/loading_cupertino.dart';
+import 'basic_widgets/fab_widget.dart';
+import 'basic_widgets/input_widget.dart';
+import 'basic_widgets/date_picker_widget.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -7,130 +11,51 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Menerapkan Widget Material Design dan iOS Cupertino',
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: const MenuUtama(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class MyTextWidget extends StatelessWidget {
-  const MyTextWidget({super.key});
+class MenuUtama extends StatelessWidget {
+  const MenuUtama({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      "Nama saya Dina Mei Lestari, sedang belajar pemrograman mobile",
-      style: TextStyle(color: Colors.red, fontSize: 14), textAlign: TextAlign.center,);
-  }
-}
-
-class MyImageWidget extends StatelessWidget {
-  const MyImageWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Image(
-      image: AssetImage("image/logo_polinema.png"), 
-      width: 200,
-    );
-  }
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-         
-          mainAxisAlignment: MainAxisAlignment.center, 
-          children: [
-            // Panggil widget baru (nama saya)
-            const MyTextWidget(),
-            const MyImageWidget(),
-            
-            const SizedBox(height: 20), // Kasih jarak sedikit biar rapi
-            
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      appBar: AppBar(title: const Text("Menu Latihan Flutter")),
+      body: ListView(
+        padding: const EdgeInsets.all(10),
+        children: [
+          // Tombol Langkah 1
+          ListTile(
+            title: const Text("Langkah 1: Loading Cupertino"),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const LoadingCupertino())),
+          ),
+          // Tombol Langkah 2
+          ListTile(
+            title: const Text("Langkah 2: FAB Widget"),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const FabWidget())),
+          ),
+          // Tombol Langkah 5
+          ListTile(
+            title: const Text("Langkah 5: TextField Widget"),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const MyInputWidget())),
+          ),
+          // Tombol Langkah 6
+          ListTile(
+            title: const Text("Langkah 6: Date Picker Widget"),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const MyDatePicker(title: "Date Picker"))),
+          ),
+        ],
       ),
     );
   }
